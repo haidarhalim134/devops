b1e0f6aaccbb392f4c87abedf5f3e9d54498830f
Yoga Fikri <95378690+Ago-oyu@users.noreply.github.com>
2026-01-01 22:19:30 +0700

    Add Dockerfile and .dockerignore; update Next.js config and route redirection

--- src/app/route.tsx
+++ src/app/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.redirect(new URL("/homepage", "http://localhost:3000"));
+  return NextResponse.redirect(new URL("/homepage", process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000"));
 }
